4b8bf2de4cc0d555562d22cf36056335ff75d588
fenggeren <380907837@qq.com>
2017-01-15 21:32:37 +0800

    RedBlack
    红黑树， 难理解
     解决红红冲突。

--- Arithmetic_C++/Arithmetic_C++/Redblack_Implementation.hpp
+++ Arithmetic_C++/Arithmetic_C++/Redblack_Implementation.hpp
@@ -7,3 +7,44 @@
 //
 
 #include "Redblack.hpp"
+#include "BinNode_macro_RedBlack.hpp"
+
+template <typename T>
+void RedBlack<T>::solveDoubleRed(BinNode<T> *x) {
+    if (IsRoot(*x)) {
+        _root->color = RB_BLACK; _root->height++;
+        return;
+    }
+    auto p = x->parent; if (IsBlack(p)) return;
+    auto g = p->parent;
+    auto u = uncle(x);
+
+    // p 是红色
+    // 根据x的叔父节点u的颜色 分别处理
+    if (IsBlack(u)) {
+        if (IsLChild(*x) == IsLChild(*p)) // x、p同侧 LL/RR
+            p->color = RB_BLACK;
+        else
+            x->color = RB_BLACK;
+        g->color = RB_RED;
+
+        auto gg = g->parent;
+        auto r = FromParentTo(*g) = rotateAt(x);
+        r->parent == g;
+    } else {
+        p->color = RB_BLACK; p->height++;
+        u->color = RB_BLACK; u->height++;
+        if (!IsRoot(*g)) g->color = RB_RED;
+        solveDoubleRed(g);
+    }
+}
+
+template <typename T>
+void RedBlack<T>::solveDoubleBlack(BinNode<T> *x) {
+
+}
+
+
+
+
+
