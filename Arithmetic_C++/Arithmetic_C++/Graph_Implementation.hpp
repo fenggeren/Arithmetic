261b62ca0c3d66802a8cabe5574df893e8f87e8b
fenggeren <huanao@huanaodeiMac.local>
2017-01-22 14:19:52 +0800

    最短路径

--- Arithmetic_C++/Arithmetic_C++/Graph_Implementation.hpp
+++ Arithmetic_C++/Arithmetic_C++/Graph_Implementation.hpp
@@ -90,4 +90,58 @@ void Graph<Tv, Te>::BCC(int v, int & clock, Stack<Tv> & S) {
     
 }
 
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::dijkstra(int s) {
+    reset(); priority(s) = 0;
+    for (int i = 0; i < n; ++i) {
+        status(s) = VISITED;
+        if ( -1 != parent(s)) type(parent(s), s) = TREE;
+        for (int j = firstNbr(s); -1 < j; j = nextNbr(s, j)) {
+            if ( (status(j) == UNDISCOVERED) && (priority(j) > priority(s) + weight(s, j))) {
+                priority(j) = priority(s) + weight(s, j);
+            }
+        }
+
+        for (int shortest = INT_MAX, j = 0; j < n ; ++j) {
+            if ( (status(j) == UNDISCOVERED) && (shortest > priority(j))) {
+                shortest = priority(j);
+                s = j;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
